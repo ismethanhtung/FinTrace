ea5781b34226ce5adc1a68c84d083b63f5b18f77
ThanhTung <ismethanhtung@gmail.com>
2026-03-30 13:48:22 +0700

    update ip

--- next.config.js
+++ next.config.js
@@ -8,15 +8,15 @@ const nextConfig = {
     return [
       {
         source: "/api/stock/image/:symbol",
-        destination: "http://16.163.172.44/stock/image/:symbol",
+        destination: "http://52.77.72.207/stock/image/:symbol",
       },
       {
         source: "/api/stock/:path*",
-        destination: "http://16.163.172.44/:path*",
+        destination: "http://52.77.72.207/:path*",
       },
       {
         source: "/api/stock",
-        destination: "http://16.163.172.44/",
+        destination: "http://52.77.72.207/",
       },
     ];
   },
